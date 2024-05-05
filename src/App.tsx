ee132012deb27498c9a391dae6d4bb013170520a
NguyenDuyDong862003 <21130318@st.hcmuaf.edu.vn>
2024-05-05 18:37:30 +0700

    Hello e-commerce

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from "./component/Hello";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Web Thương mại điện tử - Front end - Project cuối kỳ</h1>
+            <Hello
+                name="Nguyễn Duy Đông"
+                mssv="21130318"
+            />
+            <Hello
+                name="Nguyễn Quốc Khánh"
+                mssv="21130397"
+            />
+            <Hello
+                name="Đinh Quang Linh"
+                mssv="21130421"
+            />
+        </div>
+    );
+}
+
+export default App;

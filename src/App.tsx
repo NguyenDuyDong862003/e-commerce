4ce66410665927ecd17871bc11c0c4120510c817
NguyenDuyDong862003 <21130318@st.hcmuaf.edu.vn>
2024-05-05 23:58:38 +0700

    add MenuBar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from "./component/Hello";
+import MenuBar from "./component/MenuBar";
 
 function App() {
     return (
         <div className="App">
+            <MenuBar></MenuBar>
             <h1>Web Thương mại điện tử - Front end - Project cuối kỳ</h1>
             <Hello
                 name="Nguyễn Duy Đông"
---
+++ src/component/MenuBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MenuBar(props: any) {
+    return (
+        <div>
+            <div className="feedBack">Feed back</div>
+            <div className="login">Login</div>
+            <div className="signUp">Sign up</div>
+            <div className="changeLanguage">Change language</div>
+        </div>
+    )
+}
+
+export default MenuBar;

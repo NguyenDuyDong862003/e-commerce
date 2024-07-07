0ccd1edb8d42215ad15d55888e93374b9e57a308
SolKhanh <21130397@st.hcmuaf.edu.vn>
2024-07-07 23:22:10 +0700

    Thay đổi giao diện của trang detail

--- src/component/ProductDetail.js
+++ src/component/ProductDetail.js
@@ -1,6 +1,8 @@
 import "./productDetail.css";
 import {useLoaderData, useNavigate} from "react-router-dom";
 import {products} from "../data/ProductData";
+import {useDispatch} from "react-redux";
+import {addCart, setQuantityItem} from "../store/Action";
 
 export async function loadProduct({params}) {
     console.log(params);
@@ -9,75 +11,43 @@ export async function loadProduct({params}) {
 
 export function ProductDetail() {
     const product = useLoaderData();
-    const navigate = useNavigate();
-   return (
-        <div className="card">
-            <div className="container-fliud">
-                <div className="container">
-                    <div>
-                        <button onClick={() => {
-                            navigate(-1)
-                        }} className={"btn btn-primary"} style={{float: 'left', marginRight: '10px'}}
-                        >BACK
-                        </button>
-                    </div>
-                    <div className="wrapper row">
-                        <div className="preview col-md-6">
-                            <div className="preview-pic tab-content">
-                                <div className="tab-pane active" id="pic-1"><img src={product.img}/>
-                                </div>
-                                <div className="tab-pane" id="pic-2"><img src={product.img}/></div>
-                                <div className="tab-pane" id="pic-3"><img src={product.img}/></div>
-                                <div className="tab-pane" id="pic-4"><img src={product.img}/></div>
-                            </div>
-                            <ul className="preview-thumbnail nav nav-tabs">
-                                <li className="active"><a data-target="#pic-1" data-toggle="tab"><img
-                                    src={product.img}/></a></li>
-                                <li><a data-target="#pic-2" data-toggle="tab"><img
-                                    src={product.img}/></a></li>
-                                <li><a data-target="#pic-3" data-toggle="tab"><img
-                                    src={product.img}/></a></li>
-                                <li><a data-target="#pic-4" data-toggle="tab"><img
-                                    src={product.img}/></a></li>
-                            </ul>
+    const dispatch = useDispatch();
+    const addToCart = (e) => {
+        const quantity = parseInt(document.getElementById("quantity").value);
+        dispatch(addCart(product));
+        dispatch(setQuantityItem({id: product.id, quantity:quantity }));
+    }
 
-                        </div>
-                        <div className="details col-md-6">
-                            <h3 className="product-title">{product.name}</h3>
-                            <div className="rating">
-                                <div className="stars">
-                                    <span className="fa fa-star checked"></span>
-                                    <span className="fa fa-star checked"></span>
-                                    <span className="fa fa-star checked"></span>
-                                    <span className="fa fa-star"></span>
-                                    <span className="fa fa-star"></span>
-                                </div>
-                                <span className="review-no">41 reviews</span>
-                            </div>
-                            <div>
-                                {product.des}
-                            </div>
-                            {/*<h5 className="sizes">sizes:*/}
-                            {/*    <span className="size" data-toggle="tooltip" title="small">s</span>*/}
-                            {/*    <span className="size" data-toggle="tooltip" title="medium">m</span>*/}
-                            {/*    <span className="size" data-toggle="tooltip" title="large">l</span>*/}
-                            {/*    <span className="size" data-toggle="tooltip" title="xtra large">xl</span>*/}
-                            {/*</h5>*/}
-                            {/*<h5 className="colors">colors:*/}
-                            {/*    <span className="color orange not-available" data-toggle="tooltip"*/}
-                            {/*          title="Not In store"></span>*/}
-                            {/*    <span className="color green"></span>*/}
-                            {/*    <span className="color blue"></span>*/}
-                            {/*</h5>*/}
-                            <div className="action">
-                                <button className="add-to-cart btn btn-default" type="button">add to cart</button>
-                                <button className="like btn btn-default" type="button"><span
-                                    className="fa fa-heart"></span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+    return (
+
+        <div className="product-detail">
+            <div className="left-column">
+                <img src={product.img} className="large-image" alt="Product"/>
+                <div className="thumbnail-images">
+                    <img src={product.img} alt="Thumbnail 1"/>
+                    <img src={product.img} alt="Thumbnail 2"/>
+                    <img src={product.img} alt="Thumbnail 3"/>
                 </div>
             </div>
-        </div>);
+
+            <div className="right-column">
+                <div className="details">
+                    <h2 className="product-name">{product.name} - {product.category}</h2>
+
+                    <p className="price">{product.price} VNĐ</p>
+                    <div className="quantity-selector">
+                        <label htmlFor="quantity">Số lượng: </label>
+                        <input type="number" id="quantity" name="quantity" defaultValue="1" min="1"/>
+                    </div>
+
+                    <button onClick ={addToCart} className="btn-add-to-cart">Thêm vào giỏ hàng</button>
+
+                    <div className="product-info">
+                        <p>Mô tả: {product.des}</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 }

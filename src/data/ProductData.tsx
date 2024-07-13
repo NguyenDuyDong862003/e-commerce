dafba33127dc0d18b55fae2f330d59da64baa1a2
SolKhanh <21130397@st.hcmuaf.edu.vn>
2024-07-14 00:25:52 +0700

    Chuyển từ js sang ts, cập nhật thêm 1 số món ăn

---
+++ src/data/ProductData.tsx
@@ -0,0 +1,167 @@
+export const products: {
+    id: number;
+    name: string;
+    img: string;
+    des: string;
+    price: number;
+    category: string;
+}[] = [
+    {
+        id: 1,
+        name: "Cơm Tấm Sườn Trứng",
+        img: "https://i.pinimg.com/564x/73/ed/94/73ed9464b233e06a428c7cad74fa5af8.jpg",
+        des: "",
+        price: 35000,
+        category: "Món chính"
+    },
+    {
+        id: 2,
+        name: "Cơm Gà Xối Mỡ",
+        img: "https://i.pinimg.com/564x/97/1e/59/971e5982c1bada5dba732c0888ac63b0.jpg",
+        des: "",
+        price: 35000,
+        category: "Món chính"
+    },
+    {
+        id: 3,
+        name: "Hủ Tiếu",
+        img: "https://i.pinimg.com/236x/f6/c4/93/f6c4939d93aac67ebd29879108fb2c81.jpg",
+        des: "",
+        price: 35000,
+        category: "Món chính"
+    },
+    {
+        id: 4,
+        name: "Trà sữa truyền thống",
+        img: "https://i.pinimg.com/564x/4b/96/75/4b9675dfa453afb780b5d279bf27606d.jpg",
+        des: "",
+        price: 28000,
+        category: "Đồ uống"
+    },
+    {
+        id: 5,
+        name: "Bún Chả",
+        img: "https://i.pinimg.com/564x/9b/21/24/9b212422658606824bd23b23ac41332e.jpg",
+        des: "",
+        price: 35000,
+        category: "Món chính"
+    },
+    {
+        id: 6,
+        name: "Phở Bò",
+        img: "https://i.pinimg.com/564x/8c/e5/f8/8ce5f8f6be5bef52542fd1aa5a12aca3.jpg",
+        des: "",
+        price: 35000,
+        category: "Món chính"
+    },
+    {
+        id: 7,
+        name: "Bánh Mì Thịt Nướng",
+        img: "https://i.pinimg.com/564x/04/90/a8/0490a872d17e651cab2120e64535336d.jpg",
+        des: "",
+        price: 25000,
+        category: "Món chính"
+    },
+    {
+        id: 8,
+        name: "Gỏi Cuốn",
+        img: "https://i.pinimg.com/564x/5c/88/80/5c8880c9e9243146f22468f9522a7eb4.jpg",
+        des: "",
+        price: 20000,
+        category: "Món phụ"
+    },
+    {
+        id: 9,
+        name: "Chè Ba Màu",
+        img: "https://i.pinimg.com/564x/37/13/87/371387cd4dc78745187c97847cb453fd.jpg",
+        des: "",
+        price: 15000,
+        category: "Tráng miệng"
+    }, {
+        id: 10,
+        name: "Nước Mía",
+        img: "https://i.pinimg.com/564x/80/2e/c6/802ec69c98be76558e6aea7fd6bd0265.jpg",
+        des: "",
+        price: 10000,
+        category: "Đồ uống"
+    }, {
+        id: 11,
+        name: "Cơm Chay",
+        img: "https://i.pinimg.com/564x/60/b4/83/60b4834f8df5cf21419250bb3fd05698.jpg",
+        des: "",
+        price: 25000,
+        category: "Món chính"
+    },
+    {
+        id: 12,
+        name: "Bún Bò Huế",
+        img: "https://i.pinimg.com/564x/b0/d6/70/b0d670952f10cbeb1fd9ae0ffeeb2672.jpg",
+        des: "",
+        price: 35000,
+        category: "Món chính"
+    },
+    {
+        id: 13,
+        name: "Gỏi Đu Đủ",
+        img: "https://i.pinimg.com/736x/74/54/c5/7454c508db9ccdb7dca8b09f12966a65.jpg",
+        des: "",
+        price: 25000,
+        category: "Món phụ"
+    },
+    {
+        id: 14,
+        name: "Bánh Cuốn",
+        img: "https://i.pinimg.com/564x/e9/3c/1c/e93c1c91242c1ffbfcec049092c73a15.jpg",
+        des: "",
+        price: 30000,
+        category: "Món phụ"
+    },
+    {
+        id: 15,
+        name: "Súp Cua",
+        img: "https://i.pinimg.com/564x/c6/16/5c/c6165cd63077819953b716fc801b993c.jpg",
+        des: "",
+        price: 20000,
+        category: "Món phụ"
+    },
+    {
+        id: 16,
+        name: "Chè Thái",
+        img: "https://i.pinimg.com/564x/2d/1f/ec/2d1fecdf1ea4ddb4cc7a5f009fd01e81.jpg",
+        des: "",
+        price: 15000,
+        category: "Tráng miệng"
+    },
+    {
+        id: 17,
+        name: "Sinh Tố Bơ",
+        img: "https://i.pinimg.com/564x/18/d9/bc/18d9bc1f47a48120ab55448f983996c9.jpg",
+        des: "",
+        price: 25000,
+        category: "Đồ uống"
+    },
+    {
+        id: 18,
+        name: "Trà Đào",
+        img: "https://i.pinimg.com/564x/13/29/2f/13292f0a2609c317a663c29128414934.jpg",
+        des: "",
+        price: 20000,
+        category: "Đồ uống"
+    },
+    {
+        id: 19,
+        name: "Bò lúc lắc",
+        img: "https://i.pinimg.com/564x/20/c0/eb/20c0ebf99c595dc0196b7bb465d6c3d2.jpg",
+        des: "",
+        price: 35000,
+        category: "Món chính"
+    },
+    {
+        id: 20,
+        name: "Rau muống xào tỏi",
+        img: "https://i.pinimg.com/564x/0b/1b/c4/0b1bc42ffbb838354fb916e82035abc8.jpg",
+        des: "",
+        price: 35000,
+        category: "Món phụ"
+    },
+];

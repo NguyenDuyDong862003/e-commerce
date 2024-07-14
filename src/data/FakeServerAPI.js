faa7431c8bb96ff34226c78eb0fce83c546374ee
NguyenDuyDong862003 <21130318@st.hcmuaf.edu.vn>
2024-07-14 10:43:58 +0700

    thêm 1 số hàm fake API

--- src/data/FakeServerAPI.js
+++ src/data/FakeServerAPI.js
@@ -10,6 +10,14 @@ export function getTotalPageProduct() {
     return Math.ceil(sizeProduct * 1.0 / MAX_PRODUCT_PER_PAGE);
 }
 
+export function getTotalPageOfList(list) {
+    let sizeProduct = list.length
+    if (sizeProduct === 0) {
+        return 1;
+    }
+    return Math.ceil(sizeProduct * 1.0 / MAX_PRODUCT_PER_PAGE);
+}
+
 export default function getDataAtPage(page) {
     let result = [];
 
@@ -27,6 +35,28 @@ export default function getDataAtPage(page) {
 
     for (let i = indexStart; i < indexEnd; i++) {
         result.push(products[i]);
+    }
+
+    return result;
+}
+
+export default function getDataAtPageOfList(list, page) {
+    let result = [];
+
+    let totalPage = getTotalPageOfList(list);
+    if (page <= 0)
+        page = totalPage;
+    if (page > totalPage)
+        page = totalPage;
+
+    let indexStart = (page - 1) * MAX_PRODUCT_PER_PAGE;
+    let indexEnd = page * MAX_PRODUCT_PER_PAGE;
+
+    if (list.length < indexEnd)
+        indexEnd = list.length;
+
+    for (let i = indexStart; i < indexEnd; i++) {
+        result.push(list[i]);
     }
 
     return result;

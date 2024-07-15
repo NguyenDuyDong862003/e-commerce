762715246dd2503018808a17693225d9d539d2eb
SolKhanh <21130397@st.hcmuaf.edu.vn>
2024-07-16 02:39:55 +0700

    mô tả các món ăn

--- src/data/ProductData.tsx
+++ src/data/ProductData.tsx
@@ -10,7 +10,8 @@ export const products: {
         id: 1,
         name: "Cơm Tấm Sườn Trứng",
         img: "https://i.pinimg.com/564x/73/ed/94/73ed9464b233e06a428c7cad74fa5af8.jpg",
-        des: "",
+        des: "Cơm tấm dẻo thơm, sườn nướng mềm đậm đà, trứng ốp la béo ngậy.\n" +
+            "Ăn kèm dưa leo, cà chua, hành phi và nước mắm chua ngọt.",
         price: 35000,
         category: "Món chính"
     },
@@ -18,7 +19,8 @@ export const products: {
         id: 2,
         name: "Cơm Gà Xối Mỡ",
         img: "https://i.pinimg.com/564x/97/1e/59/971e5982c1bada5dba732c0888ac63b0.jpg",
-        des: "",
+        des: "Gà tẩm ướp gia vị đậm đà, chiên vàng giòn.\n" +
+            "Ăn kèm cơm trắng, dưa leo, cà chua, hành phi và nước mắm chua ngọt.",
         price: 35000,
         category: "Món chính"
     },
@@ -26,7 +28,8 @@ export const products: {
         id: 3,
         name: "Hủ Tiếu",
         img: "https://i.pinimg.com/236x/f6/c4/93/f6c4939d93aac67ebd29879108fb2c81.jpg",
-        des: "",
+        des: "Sợi hủ tiếu dai dai, nước dùng ngọt thanh.\n" +
+            "Ăn kèm thịt heo quay, tôm, mực, giá đỗ, hẹ, rau thơm và chanh ớt.",
         price: 35000,
         category: "Món chính"
     },
@@ -34,7 +37,7 @@ export const products: {
         id: 4,
         name: "Trà sữa truyền thống",
         img: "https://i.pinimg.com/564x/4b/96/75/4b9675dfa453afb780b5d279bf27606d.jpg",
-        des: "",
+        des: "Trà sữa pha với bột trà, sữa tươi, trân châu dai dai, ngọt thanh.\n",
         price: 28000,
         category: "Đồ uống"
     },
@@ -42,7 +45,8 @@ export const products: {
         id: 5,
         name: "Bún Chả",
         img: "https://i.pinimg.com/564x/9b/21/24/9b212422658606824bd23b23ac41332e.jpg",
-        des: "",
+        des: "Thịt nướng thơm lừng.\n" +
+            "Ăn kèm bún rối, nước chấm chua ngọt, rau sống và dưa góp.",
         price: 35000,
         category: "Món chính"
     },
@@ -50,7 +54,8 @@ export const products: {
         id: 6,
         name: "Phở Bò",
         img: "https://i.pinimg.com/564x/8c/e5/f8/8ce5f8f6be5bef52542fd1aa5a12aca3.jpg",
-        des: "",
+        des: "Bánh phở mềm mịn, nước dùng bò đậm đà, thịt bò tái hoặc nạm mềm thơm.\n" +
+            "Ăn kèm giá đỗ, húng quế, rau thơm và chanh ớt.\n",
         price: 35000,
         category: "Món chính"
     },
@@ -58,7 +63,7 @@ export const products: {
         id: 7,
         name: "Bánh Mì Thịt Nướng",
         img: "https://i.pinimg.com/564x/04/90/a8/0490a872d17e651cab2120e64535336d.jpg",
-        des: "",
+        des: "Bánh mì giòn tan, kẹp thịt nướng thơm lừng, pate béo ngậy, dưa chua, rau thơm và nước sốt đậm đà.",
         price: 25000,
         category: "Món chính"
     },
@@ -66,7 +71,8 @@ export const products: {
         id: 8,
         name: "Gỏi Cuốn",
         img: "https://i.pinimg.com/564x/5c/88/80/5c8880c9e9243146f22468f9522a7eb4.jpg",
-        des: "",
+        des: "Bánh tráng cuốn nhân tôm thịt, bún, rau sống.\n" +
+            "Chấm cùng nước mắm chua ngọt.",
         price: 20000,
         category: "Món phụ"
     },
@@ -74,21 +80,22 @@ export const products: {
         id: 9,
         name: "Chè Ba Màu",
         img: "https://i.pinimg.com/564x/37/13/87/371387cd4dc78745187c97847cb453fd.jpg",
-        des: "",
+        des: "Thạch dừa, khoai môn, khoai dẻo, nước cốt dừa béo ngậy.\n" +
+            "Ăn kèm đá bào.",
         price: 15000,
         category: "Tráng miệng"
     }, {
         id: 10,
         name: "Nước Mía",
         img: "https://i.pinimg.com/564x/80/2e/c6/802ec69c98be76558e6aea7fd6bd0265.jpg",
-        des: "",
+        des: "Nước mía nguyên chất, thanh mát, giải khát.",
         price: 10000,
         category: "Đồ uống"
     }, {
         id: 11,
         name: "Cơm Chay",
         img: "https://i.pinimg.com/564x/60/b4/83/60b4834f8df5cf21419250bb3fd05698.jpg",
-        des: "",
+        des: "Món cơm chay thanh đạm với nhiều lựa chọn món ăn chay đa dạng như: đậu hũ kho, rau củ xào, canh rau, gỏi chay,...",
         price: 25000,
         category: "Món chính"
     },
@@ -96,7 +103,8 @@ export const products: {
         id: 12,
         name: "Bún Bò Huế",
         img: "https://i.pinimg.com/564x/b0/d6/70/b0d670952f10cbeb1fd9ae0ffeeb2672.jpg",
-        des: "",
+        des: "Sợi bún dai, nước dùng cay nồng, thịt bò, chả cua, huyết heo.\n" +
+            "Ăn kèm rau sống, giá đỗ, hẹ và chanh ớt.",
         price: 35000,
         category: "Món chính"
     },
@@ -104,7 +112,8 @@ export const products: {
         id: 13,
         name: "Gỏi Đu Đủ",
         img: "https://i.pinimg.com/736x/74/54/c5/7454c508db9ccdb7dca8b09f12966a65.jpg",
-        des: "",
+        des: "Gỏi đu đủ với đu đủ bào sợi, tai heo, tôm khô, thịt ba chỉ, đậu phộng rang.\n" +
+            "Ăn kèm nước mắm chua ngọt và ớt.",
         price: 25000,
         category: "Món phụ"
     },
@@ -128,7 +137,8 @@ export const products: {
         id: 16,
         name: "Chè Thái",
         img: "https://i.pinimg.com/564x/2d/1f/ec/2d1fecdf1ea4ddb4cc7a5f009fd01e81.jpg",
-        des: "",
+        des: "Thạch dừa, sầu riêng, mít, nhãn, nước cốt dừa béo ngậy.\n" +
+            "Ăn kèm đá bào.",
         price: 15000,
         category: "Tráng miệng"
     },
@@ -136,7 +146,7 @@ export const products: {
         id: 17,
         name: "Sinh Tố Bơ",
         img: "https://i.pinimg.com/564x/18/d9/bc/18d9bc1f47a48120ab55448f983996c9.jpg",
-        des: "",
+        des: "Sinh tố bơ béo ngậy, thơm ngon, tốt cho sức khỏe.",
         price: 25000,
         category: "Đồ uống"
     },
@@ -144,26 +154,29 @@ export const products: {
         id: 18,
         name: "Trà Đào",
         img: "https://i.pinimg.com/564x/13/29/2f/13292f0a2609c317a663c29128414934.jpg",
-        des: "",
+        des: "Trà xanh, đào tươi, thanh mát, giải khát.",
         price: 20000,
         category: "Đồ uống"
     },
     {
         id: 19,
-        name: "Bò lúc lắc",
-        img: "https://i.pinimg.com/564x/20/c0/eb/20c0ebf99c595dc0196b7bb465d6c3d2.jpg",
-        des: "",
+        name: "Bò Lúc Lắc",
+        img: "https://i.pinimg.com/564x/4e/32/e9/4e32e943fad2e836f86df998e6b71840.jpg",
+        des: "Thịt bò mềm xào cùng ớt chuông, hành tây, nấm, cà rốt.\n" +
+            "Ăn kèm khoai tây chiên hoặc cơm trắng.",
         price: 35000,
         category: "Món chính"
     },
     {
         id: 20,
-        name: "Rau muống xào tỏi",
+        name: "Rau Muống Xào Tỏi",
         img: "https://i.pinimg.com/564x/0b/1b/c4/0b1bc42ffbb838354fb916e82035abc8.jpg",
-        des: "",
+        des: "Rau muống xào tỏi xanh giòn, thanh mát.\n" +
+            "Ăn kèm cơm trắng hoặc canh.",
         price: 35000,
         category: "Món phụ"
     },
+
 ];
 
 console.log("in thử data")

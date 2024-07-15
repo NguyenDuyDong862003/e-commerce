fac89660000bcfc15bf60e29447fbe1bc1b9c97d
SolKhanh <21130397@st.hcmuaf.edu.vn>
2024-07-16 02:58:48 +0700

    .

--- src/Router/web.js
+++ src/Router/web.js
@@ -1,6 +1,5 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../App";
-import {ProductList2} from "../component/ProductList";
 import {loadProduct, ProductDetail} from "../component/ProductDetail";
 import Error from "../component/Error";
 import Login from "../pages/Login";
@@ -8,9 +7,7 @@ import Signup from "../pages/Signup";
 import Cart from "../component/Cart";
 import Checkout from "../component/Checkout";
 import {Category} from "../component/Category";
-import {products} from "../data/ProductData";
-import {Shop} from "../component/Shop";
-import {SearchBar} from "../component/SearchBar";
+import {Shop} from "../pages/Shop";
 import {SearchResult} from "../component/SearchResult";
 
 
@@ -22,7 +19,6 @@ export const router = createBrowserRouter([
         children: [
             {
                 path: 'list-product',
-                // element: <ProductList2 listProduct={products}/>
                 element: <Shop/>
             }, {
                 path: 'product/:id',

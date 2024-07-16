915a901c35e5d52b52082143f113748b87de34e1
SolKhanh <21130397@st.hcmuaf.edu.vn>
2024-07-16 20:09:44 +0700

    Thêm 28 sản phẩm

--- src/data/ProductData.tsx
+++ src/data/ProductData.tsx
@@ -176,7 +176,232 @@ export const products: {
         price: 35000,
         category: "Món phụ"
     },
-
+    {
+        id: 21,
+        name: "Bánh Flan",
+        img: "https://i.pinimg.com/564x/93/af/35/93af35792cb4c4892173d0786cf95743.jpg",
+        des: "Bánh flan mềm mịn, ngọt thanh, được làm từ sữa tươi và trứng gà, ăn kèm với cà phê đắng và đá bào.",
+        price: 20000,
+        category: "Tráng miệng"
+    },
+    {
+        id: 22,
+        name: "Kem Chuối",
+        img: "https://i.pinimg.com/564x/49/7f/70/497f70adb2984eb82c3dd41381ca20f5.jpg",
+        des: "Kem chuối thơm ngon, mát lạnh, kết hợp giữa chuối chín, sữa dừa và đậu phộng rang giòn.",
+        price: 20000,
+        category: "Tráng miệng"
+    },
+    {
+        id: 23,
+        name: "Xôi Mặn",
+        img: "https://i.pinimg.com/564x/a7/97/c6/a797c687dc4e75308611cf6fe92dd0f0.jpg",
+        des: "Xôi nếp dẻo, ăn kèm thịt kho, chả lụa, trứng cút, hành phi và nước tương.",
+        price: 25000,
+        category: "Món chính"
+    },
+    {
+        id: 24,
+        name: "Thịt Heo Kho Trứng",
+        img: "https://i.pinimg.com/564x/a4/b2/d7/a4b2d782cfa844af0ffed93920468219.jpg",
+        des: "Thịt heo kho mềm thơm, đậm đà, kết hợp với trứng vịt luộc và nước kho ngọt thanh.",
+        price: 35000,
+        category: "Món chính"
+    },
+    {
+        id: 25,
+        name: "Cá Ngừ Kho Thơm",
+        img: "https://i.pinimg.com/564x/fa/1b/d4/fa1bd4b04878f9ac272e9ce2aa68a13c.jpg",
+        des: "Cá ngừ kho thơm ngọt, thịt cá ngọt béo, thơm lừng hương thơm. Ăn kèm với cơm trắng.",
+        price: 35000,
+        category: "Món chính"
+    },
+    {
+        id: 26,
+        name: "Ốc Bươu Xào Sả Ớt",
+        img: "https://i.pinimg.com/564x/3d/08/dc/3d08dc3f4dd9617f097ddb000c3c0f64.jpg",
+        des: "Ốc bươu giòn dai, xào cùng sả và ớt cay, mang lại hương vị đậm đà và hấp dẫn.",
+        price: 35000,
+        category: "Món phụ"
+    },
+    {
+        id: 27,
+        name: "Canh Chua Cá Lóc",
+        img: "https://i.pinimg.com/564x/d7/fd/a2/d7fda2ec14e1d667aa29c87c471d57b9.jpg",
+        des: "Canh chua cá lóc với hương vị chua cay, thanh mát, kết hợp giữa cá lóc tươi và rau thơm.",
+        price: 35000,
+        category: "Món chính"
+    },
+    {
+        id: 28,
+        name: "Bò Xào Hành Tây",
+        img: "https://i.pinimg.com/564x/d4/dc/c0/d4dcc08e45fab944146d853ce6d4c5fa.jpg",
+        des: "Thịt bò mềm, xào cùng hành tây giòn, gia vị vừa ăn, tạo nên món ăn thơm ngon, bổ dưỡng.",
+        price: 35000,
+        category: "Món chính"
+    },
+    {
+        id: 29,
+        name: "Bánh Bao Nhân Thịt",
+        img: "https://i.pinimg.com/564x/7d/d3/81/7dd381be82addd79aab8976ddb3bbed3.jpg",
+        des: "Bánh bao mềm mịn, nhân thịt heo băm nhuyễn, kết hợp với trứng cút và nấm hương.",
+        price: 18000,
+        category: "Món phụ"
+    },
+    {
+        id: 30,
+        name: "Chân Gà Luộc",
+        img: "https://i.pinimg.com/564x/83/37/63/8337633449bd64f0cd2fa7b66798478b.jpg",
+        des: "Chân gà luộc giòn sần sật, thơm ngon, ăn kèm với nước chấm gừng cay.",
+        price: 20000,
+        category: "Món phụ"
+    },
+    {
+        id: 31,
+        name: "Đậu Hũ Nhồi Thịt",
+        img: "https://i.pinimg.com/564x/43/ae/57/43ae5742a6304fe6df026a9030769ded.jpg",
+        des: "Đậu hũ mềm mại, nhồi với thịt heo băm nhuyễn, sốt cà chua thơm ngon.",
+        price: 30000,
+        category: "Món chính"
+    },
+    {
+        id: 32,
+        name: "Cà Phê Sữa Đá",
+        img: "https://i.pinimg.com/564x/23/a2/18/23a2183e713b8efc565fe702f9e8470d.jpg",
+        des: "Cà phê đậm đà kết hợp với sữa đặc ngọt ngào, đá mát lạnh.",
+        price: 20000,
+        category: "Đồ uống"
+    },
+    {
+        id: 33,
+        name: "Chanh Dây",
+        img: "https://i.pinimg.com/564x/bf/d4/6d/bfd46dc4728b72d95bf67d34a7c779f5.jpg",
+        des: "Nước chanh dây tươi mát, vị chua ngọt dịu nhẹ, giải khát hiệu quả.",
+        price: 15000,
+        category: "Đồ uống"
+    },
+    {
+        id: 34,
+        name: "Coca cola",
+        img: "https://i.pinimg.com/564x/12/ba/84/12ba8476c202f9bfa31d42fcbc314214.jpg",
+        des: "Nước giải khát cola có ga, vị ngọt đậm đà, làm sảng khoái tinh thần.",
+        price: 12000,
+        category: "Đồ uống"
+    },
+    {
+        id: 35,
+        name: "Khoai Tây Chiên",
+        img: "https://i.pinimg.com/564x/dd/8d/84/dd8d844362273ec795dd229fa7ca0fca.jpg",
+        des: "Khoai tây chiên giòn rụm, ăn kèm với tương cà hoặc sốt mayonnaise.",
+        price: 30000,
+        category: "Món phụ"
+    },
+    {
+        id: 36,
+        name: "Chè Bưởi",
+        img: "https://i.pinimg.com/564x/27/be/b6/27beb65e9bc51bc73d5a0ed39af81c82.jpg",
+        des: "Chè bưởi giòn giòn, thơm ngon, kết hợp với nước cốt dừa béo ngậy.",
+        price: 20000,
+        category: "Tráng miệng"
+    },
+    {
+        id: 37,
+        name: "Kem Vani",
+        img: "https://i.pinimg.com/564x/eb/af/31/ebaf31ffc0c2c3e527ec854ea2c69739.jpg",
+        des: "Kem Vani",
+        price: 20000,
+        category: "Tráng miệng"
+    },
+    {
+        id: 38,
+        name: "Kem Dừa",
+        img: "https://i.pinimg.com/564x/91/67/f8/9167f843449d97e2940e9fd24a91572d.jpg",
+        des: "Kem dừa",
+        price: 20000,
+        category: "Tráng miệng"
+    },
+    {
+        id: 39,
+        name: "Kem Socola",
+        img: "https://i.pinimg.com/564x/7f/94/eb/7f94eba16282f37f793a502966708a7b.jpg",
+        des: "Kem Socola",
+        price: 20000,
+        category: "Tráng miệng"
+    },
+    {
+        id: 40,
+        name: "Sữa Chua Trái Cây",
+        img: "https://i.pinimg.com/564x/f5/48/11/f54811d0ef16ff29f6261b22d5546efc.jpg",
+        des: "",
+        price: 22000,
+        category: "Tráng miệng"
+    }
+    ,
+    {
+        id: 41,
+        name: "Sương Sáo",
+        img: "https://i.pinimg.com/564x/a9/37/0f/a9370f724a73cd7de44358f54cd284a5.jpg",
+        des: "Sương sáo dai mát, kết hợp với nước đường thơm lừng và nước cốt dừa béo ngậy. Ăn kèm đá bào.",
+        price: 16000,
+        category: "Đồ uống"
+    },
+    {
+        id: 42,
+        name: "Đậu Phộng Rang Tỏi Ớt",
+        img: "https://i.pinimg.com/564x/fe/f9/b0/fef9b03a58536964a575fecb32ab2cab.jpg",
+        des: "",
+        price: 20000,
+        category: "Món phụ"
+    },
+    {
+        id: 43,
+        name: "Tôm Nướng Muối Ớt",
+        img: "https://i.pinimg.com/564x/e6/0e/70/e60e70246137c4b0814897183b0318d4.jpg",
+        des: "",
+        price: 29000,
+        category: "Món phụ"
+    }
+    ,
+    {
+        id: 44,
+        name: "Cơm Cuộn",
+        img: "https://i.pinimg.com/564x/e7/b2/77/e7b27701b413c8454dcc2e9e31c62536.jpg",
+        des: "",
+        price: 25000,
+        category: "Món phụ"
+    },
+    {
+        id: 45,
+        name: "Bắp Xào",
+        img: "https://i.pinimg.com/564x/bb/08/1a/bb081a81e6c9e2d3d7ecb5313c765f8f.jpg",
+        des: "",
+        price: 26000,
+        category: "Món phụ"
+    },
+    {
+        id: 46,
+        name: "Cơm Cháy Chà Bông",
+        img: "https://i.pinimg.com/564x/eb/5f/94/eb5f94943e22fb0a2d7490ed361fa956.jpg",
+        des: "",
+        price: 20000,
+        category: "Món phụ"
+    },
+    {
+        id: 47,
+        name: "Khô Gà Lá Chanh",
+        img: "https://i.pinimg.com/564x/06/69/6b/06696ba4eb88bbce349a26b1d5f1639e.jpg",
+        des: "",
+        price: 20000,
+        category: "Món phụ"
+    },
+    {
+        id: 48,
+        name: "Nước Ép Táo",
+        img: "https://i.pinimg.com/564x/c9/8d/99/c98d99e8e1e74e4ff35b24d3777387a6.jpg",
+        des: "",
+        price: 20000,
+        category: "Đồ uống"
+    }
 ];
 
 console.log("in thử data")
